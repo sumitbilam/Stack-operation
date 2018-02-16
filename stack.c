5c7412ebcd119a86e71ae4204ae6e01f0bd51a43
sumitbilam <32146718+sumitbilam@users.noreply.github.com>
2018-02-16 13:01:33 -0500

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedListAPI.h"
+#include "stack.h"
+
+Stack *createStack(void(*printFunction)(void *toBePrinted), void(*deleteFunction)(void *toBeDelted), int(*compareFunction)(const void *first, const void *second)){
+    Stack *s = malloc(sizeof(Stack));
+    s->count = 0;
+    s->list = initializeList(printFunction, deleteFunction, compareFunction);
+    
+    return s;
+
+}
+
+
+void push(Stack *s, void *data){
+    insertFront(s->list, data);
+    s->count++;
+}
+
+
+void pop(Stack *s){
+
+    if(s){
+        void *data = top(s);
+        int result = deleteNodeFromList(s->list, data);
+        if(result == EXIT_SUCCESS){
+            s->count--;
+        }
+        
+    }
+
+}
+
+
+void *top(Stack *s){
+    if(s->list == NULL){
+        return NULL;
+    }
+    return getFromFront(s->list);
+}
+
+
+int count(Stack *s){
+    return s->count;
+}
